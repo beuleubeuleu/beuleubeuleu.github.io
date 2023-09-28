542e118f69fea24062e129602da9459d83acf978
beuleu <billyklotz789@gmail.com>
2023-09-28 16:41:52 +0200

    github pages with github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "hteeps://beuleubeuleu.github.io",
+  base: "/portfolio-astro"
+});
