3966a0ad55bd0cd8088c9e92514f6238279c6381
beuleu <billyklotz789@gmail.com>
2023-09-28 16:46:22 +0200

    typo on config url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "hteeps://beuleubeuleu.github.io",
+  site: "https://beuleubeuleu.github.io",
   base: "/portfolio-astro"
 });
