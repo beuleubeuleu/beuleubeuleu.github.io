09bc0853b15f4eadf174dfb7de9e73d02bb227fd
beuleu <billyklotz789@gmail.com>
2023-11-26 16:54:12 +0100

    advent of code article card

--- src/data/myArticles.ts
+++ src/data/myArticles.ts
@@ -1,8 +1,17 @@
-import atikeHome   from "../assets/projets/atike/atike-home.png";
-import educaHome   from "../assets/projets/educa/educa-home.png";
-import nohasbaHome from "../assets/projets/nohasba/nohasba-home.png";
+import aoc2023Home from "../assets/blog/aoc2023/aoc2023-home.png"
+import atikeHome   from "../assets/blog/atike/atike-home.png";
+import educaHome   from "../assets/blog/educa/educa-home.png";
+import nohasbaHome from "../assets/blog/nohasba/nohasba-home.png";
 
 export const myArticles = [
+  {
+    titre: "Advent of Code 2023",
+    techStack: ["Java", "Golang", "Haskell"],
+    href: "/blog/aoc2023/",
+    img: aoc2023Home,
+    description: "Paradigme de programmation",
+    type: "Expérience",
+  },
   {
     titre: "Atikè",
     techStack: ["AstroJS", "AWS"],
@@ -12,19 +21,19 @@ export const myArticles = [
     type: "Projet",
   },
   {
-    titre: "Educa",
-    techStack: ["AstroJS", "React", "MongoDB"],
-    href: "/blog/educa/",
-    img: educaHome,
-    description: "Blog",
-    type: "Projet",
-  },
-  {
     titre: "NoHasba",
     techStack: ["React", "MySQL"],
     href: "/blog/nohasba/",
     img: nohasbaHome,
     description: "Gestionnaire de dépenses",
+    type: "Projet",
+  },
+  {
+    titre: "Educa",
+    techStack: ["AstroJS", "React", "MongoDB"],
+    href: "/blog/educa/",
+    img: educaHome,
+    description: "Blog",
     type: "Projet",
   }
 ]
