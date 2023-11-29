cabd4f1119d0c50dde15d33d48630e65242aee50
beuleu <billyklotz789@gmail.com>
2023-11-29 16:10:07 +0100

    projet en cours on front page

---
+++ src/data/projetEnCours.ts
@@ -0,0 +1,43 @@
+import aoc2023Home from "../assets/blog/aoc2023/aoc2023-home.png";
+import atikeHome   from "../assets/blog/atike/atike-home.png";
+import nohasbaHome from "../assets/blog/nohasba/nohasba-home.png";
+import educaHome   from "../assets/blog/educa/educa-home.png";
+
+export const projetEnCours = [
+  {
+    titre: "Advent of Code 2023",
+    techStack: ["Java", "Golang", "Haskell"],
+    href: "/blog/aoc2023/",
+    img: aoc2023Home,
+    description: "Paradigme de programmation",
+    type: "Expérience",
+    id: "wip-412344"
+  },
+  {
+    titre: "Advent of Craft",
+    techStack: ["Java"],
+    href: "",
+    img: "",
+    description: "Challenge craft quotidien",
+    type: "Expérience",
+    id: "wip-523425"
+  },
+  {
+    titre: "Habit/Goal Tracker",
+    techStack: ["Spring"],
+    href: "",
+    img: nohasbaHome,
+    description: "Découverte DDD",
+    type: "Projet",
+    id: "wip-123424"
+  },
+  {
+    titre: "Refactoring SOLID Educa",
+    techStack: ["React", "NodeJS", "MongoDB"],
+    href: "",
+    img: "",
+    description: "Principes SOLID",
+    type: "Expérience/Projet",
+    id: "wip-534153"
+  }
+]
