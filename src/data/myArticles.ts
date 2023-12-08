a744b07416b9868c1900652ddad965a23b37349d
beuleu <billyklotz789@gmail.com>
2023-12-08 12:15:48 +0100

    filter feature rollback; more blog content

--- src/data/myArticles.ts
+++ src/data/myArticles.ts
@@ -4,15 +4,15 @@ import educaHome   from "../assets/blog/educa/educa-home.png";
 import nohasbaHome from "../assets/blog/nohasba/nohasba-home.png";
 
 export const myArticles = [
-  // {
-  //   titre: "Advent of Code 2023",
-  //   techStack: ["Java", "Golang", "Haskell"],
-  //   href: "/blog/aoc2023/",
-  //   img: aoc2023Home,
-  //   description: "Paradigme de programmation",
-  //   type: "Expérience",
-  //   id:"article-412442"
-  // },
+  {
+    titre: "Advent of Code 2023",
+    techStack: ["Java", "Golang", "Haskell"],
+    href: "/blog/aoc2023/",
+    img: aoc2023Home,
+    description: "Paradigme de programmation",
+    type: "Expérience",
+    id:"article-412442"
+  },
   {
     titre: "Atikè",
     techStack: ["AstroJS", "AWS"],
@@ -39,5 +39,41 @@ export const myArticles = [
     description: "Blog",
     type: "Projet",
     id:"article-132356"
-  }
+  },
+  {
+    titre: "Habit/Goal Tracker",
+    techStack: ["NextJS", "NestJS", "PostgreSQL", "Docker"],
+    href: "",
+    img: "",
+    description: "Découverte DDD",
+    type: "Projet",
+    id: "wip-123424"
+  },
+  {
+    titre: "Refactoring SOLID Educa",
+    techStack: ["React", "NodeJS", "MongoDB"],
+    href: "",
+    img: "",
+    description: "Principes SOLID",
+    type: "Expérience/Projet",
+    id: "wip-534153"
+  },
+  {
+    titre: "Advent of Craft",
+    techStack: ["Java"],
+    href: "",
+    img: "",
+    description: "Challenge craft quotidien",
+    type: "Expérience",
+    id: "wip-523425"
+  },
+   {
+   titre: "Framework d'apprentissage",
+   techStack: [],
+   href: "",
+   img: "",
+   description: "DDD OpenSource",
+   type: "Expérience/Projet",
+   id: "wip-541247"
+   }
 ]
